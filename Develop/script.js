c746c09bd2584808f2ad93cfff098c0f160efd89
christopher gonzalez <christopher.gonzalez98@gmail.com>
2023-01-02 06:22:32 -0600

    created a function to display current time and date

--- Develop/script.js
+++ Develop/script.js
@@ -20,4 +20,16 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  $(function(){
+    setInterval(currentTime, 1000);
+});
+
+function currentTime() {
+var timeDate = dayjs().format( "MMM D, YYYY " + " h:mm:ss")
+$("#currentDay").text(timeDate);
+
+//var time = dayjs().format("h:mm:ss");
+
+//$("#clock").text(time);
+}
 });
